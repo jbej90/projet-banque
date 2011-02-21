d039a17856bea1be5929623fb069827c678fb02a
elaaboudi.meriem@gmail.com <elaaboudi.meriem@gmail.com@102aa2ed-609a-e183-7066-b10850d0009c>
2011-02-21 14:22:41 +0000

    Refacto

---
+++ projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/handler/RoleAuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.excilys.projet.banque.web.handler;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+
+public class RoleAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+	private final static String ROLE_ADMIN = "ROLE_ADMIN";
+	private final static String ROLE_USER = "ROLE_USER";
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+		Collection<GrantedAuthority> authorities = authentication.getAuthorities();
+		for (GrantedAuthority authority : authorities) {
+			if (authority.getAuthority().equals(ROLE_ADMIN)) {
+				response.sendRedirect(response.encodeRedirectURL("pages/admin/admin.jsp"));
+				return;
+			} else if (authority.getAuthority().equals(ROLE_USER)) {
+				response.sendRedirect(response.encodeRedirectURL("pages/private/home.jsp"));
+				return;
+			}
+		}
+		
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+}
