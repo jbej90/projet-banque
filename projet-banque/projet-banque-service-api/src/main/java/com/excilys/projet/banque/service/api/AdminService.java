b29aa0b7e22ef29f4f8316e7c3994d06e49f04a9
elaaboudi.meriem@gmail.com <elaaboudi.meriem@gmail.com@102aa2ed-609a-e183-7066-b10850d0009c>
2011-02-21 16:03:15 +0000

    Service: recuperation de liste de comptes par client

--- projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/AdminService.java
+++ projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/AdminService.java
@@ -1,7 +1,11 @@
 package com.excilys.projet.banque.service.api;
 
+import com.excilys.projet.banque.model.Client;
+
 public interface AdminService {
 
-	// void ajouterClient(Client client);
+	void ajouterClient(Client client);
+
+	void supprimerClient(Client client);
 
 }
--- projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/ClientService.java
+++ projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/ClientService.java
@@ -1,7 +1,11 @@
 package com.excilys.projet.banque.service.api;
 
+import java.util.List;
+
+import com.excilys.projet.banque.model.Client;
+import com.excilys.projet.banque.model.Compte;
+
 public interface ClientService {
 
-	void afficherProfil();
-
+	List<Compte> recupererListeComptes(Client client);
 }
