7b2b716eeaa9cb958658bcb9e752d1cbb37967d8
damien.villeneuve.44@gmail.com <damien.villeneuve.44@gmail.com@102aa2ed-609a-e183-7066-b10850d0009c>
2011-02-23 16:34:24 +0000

    Check sur l'erreur de login Légère modif sur les styles d'affichage des erreurs

--- projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/handler/FailureLoginHandler.java
+++ projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/handler/FailureLoginHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -14,9 +15,11 @@ import com.excilys.projet.banque.web.utils.MessageStack;
 public class FailureLoginHandler implements AuthenticationFailureHandler {
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-		System.out.println(exception.getMessage());
-		
-		MessageStack.getInstance(request).addError("Login ou mot de passe erroné");
+		if (exception instanceof BadCredentialsException) {
+			MessageStack.getInstance(request).addError("Login ou mot de passe erroné");
+		} else {
+			MessageStack.getInstance(request).addError("Connexion échouée");
+		}
 		
 		response.sendRedirect(response.encodeRedirectURL("login.htm"));
 	}
