09dcf70308b571ba437e58af22c4abadcc6f05be
damien.villeneuve.44 <damien.villeneuve.44@gmail.com>
2011-03-08 17:08:57 +0000

    Corrections Ajax Affichage des virements en cours

--- projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/exceptions/ServiceException.java
+++ projet-banque/projet-banque-service-api/src/main/java/com/excilys/projet/banque/service/api/exceptions/ServiceException.java
@@ -10,10 +10,4 @@ public class ServiceException extends Exception {
 	public ServiceException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
-	@Override
-	public String getMessage() {
-		return "Service Exception: " + super.getMessage();
-	}
-
 }
