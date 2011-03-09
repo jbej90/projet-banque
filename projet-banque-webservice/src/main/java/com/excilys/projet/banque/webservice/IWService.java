c28f446731d36c2452bafa508be1001518773473
athomas.nc <athomas.nc@gmail.com>
2011-03-09 15:05:29 +0000

    Mise en place des converters (POJO to DTO)

--- projet-banque-webservice/src/main/java/com/excilys/projet/banque/webservice/IWService.java
+++ projet-banque-webservice/src/main/java/com/excilys/projet/banque/webservice/IWService.java
@@ -1,13 +1,18 @@
 package com.excilys.projet.banque.webservice;
 
+import java.util.List;
+
 import javax.jws.WebService;
+
+import com.excilys.projet.banque.webservice.dto.CompteDTO;
+import com.excilys.projet.banque.webservice.dto.OperationDTO;
 
 @WebService
 public interface IWService {
 	
-	String consultationComptes(int idClient);
-	String consultationCompte(int idClient, int idCompte);
-	String consultationOperations(int idCompte, int idClient);
-	String passerOperation(int idCompte, float montant);
+	List<CompteDTO> consultationComptes(int idClient, String login, String password);
+	CompteDTO consultationCompte(int idClient, int idCompte);
+	List<OperationDTO> consultationOperations(int idCompte, int idClient);
+	void passerOperation(int idCompteSource, int idCompteDestination, float montant);
 	
 }
