438619de84050f9376d53547eff0985ffa390667
damien.villeneuve.44@gmail.com <damien.villeneuve.44@gmail.com@102aa2ed-609a-e183-7066-b10850d0009c>
2011-03-11 17:04:50 +0000

    Mise à jour export Excel

--- projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/view/ExcelCompteView.java
+++ projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/view/ExcelCompteView.java
@@ -1,20 +1,19 @@
 package com.excilys.projet.banque.web.view;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.hssf.record.DataFormatRecord;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFDataFormat;
+import org.apache.poi.hssf.usermodel.HSSFPalette;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
 
 import com.excilys.projet.banque.model.Compte;
@@ -26,21 +25,12 @@ public class ExcelCompteView extends AbstractExcelView {
 	@Override
 	protected void buildExcelDocument(Map<String, Object> model, HSSFWorkbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HSSFSheet sheet;
-		HSSFRow sheetRow;
-		HSSFCell cell;
-		
-		// Format numéraire
-		short montantFormat = HSSFDataFormat.getBuiltinFormat("#€,##0.00");
-		
-		// Style numéraire découvert
-		HSSFCellStyle styleMontantDecouvert = workbook.createCellStyle();
-		styleMontantDecouvert.setFillForegroundColor(HSSFColor.RED.index);
-		styleMontantDecouvert.setDataFormat(montantFormat);
-		
-		// Style numéraire positif
-		HSSFCellStyle styleMontant = workbook.createCellStyle();
-		styleMontant.setDataFormat(montantFormat);
-		
+
+		// creating a custom palette for the workbook
+		HSSFPalette palette = workbook.getCustomPalette();
+		palette.setColorAtIndex(HSSFColor.BLUE_GREY.index, (byte) 249, (byte) 249, (byte) 249);
+		palette.setColorAtIndex(HSSFColor.GREEN.index, (byte) 255, (byte) 255, (byte) 255);
+		palette.setColorAtIndex(HSSFColor.GREY_40_PERCENT.index, (byte) 187, (byte) 187, (byte) 187);
 
 		Compte compte = (Compte) model.get("compte");
 		List<Operation> operations = (List<Operation>) model.get("operations");
@@ -49,32 +39,51 @@ public class ExcelCompteView extends AbstractExcelView {
 		float soustotalCarte = (Float) model.get("soustotalCarte");
 		float total = (Float) model.get("total");
 		String[] listemois = (String[]) model.get("listemois");
-		
-		SimpleDateFormat df = new SimpleDateFormat("dd/m/yy");
+
+		HSSFRow row;
 
 		// Go to the first sheet
 		// getSheetAt: only if wb is created from an existing document
 		sheet = workbook.getSheetAt(0);
-//		sheet = workbook.createSheet("Spring");
-//		sheet.setDefaultColumnWidth(12);
+		// sheet = workbook.createSheet(compte.getLibelle());
+		// sheet.setDefaultColumnWidth(12);
+
+		getCell(sheet, 2, 2).setCellValue(compte.getLibelle());
+		getCell(sheet, 3, 2).setCellValue(compte.getSolde());
 
 		int i = 0;
 		for (Operation operation : operations) {
-			setText(getCell(sheet, 6 + i, 0), df.format(operation.getDateOp()));
-			setText(getCell(sheet, 6 + i, 1), operation.getLibelle());
-			setText(getCell(sheet, 6 + i, 2), operation.getType().name());
-			
+			row = sheet.createRow(6 + i);
+
+			getCellOperation(row, 0).setCellValue(operation.getDateOp());
+			getCellOperation(row, 1).setCellValue(operation.getLibelle());
+			getCellOperation(row, 2).setCellValue(operation.getType().name());
+
 			if (operation.getMontant() < 0) {
-				cell = getCell(sheet, 6 + i, 3);
-				cell.setCellStyle(styleMontantDecouvert);
-				setText(cell, String.valueOf(operation.getMontant()));
-			} else {
-				cell = getCell(sheet, 6 + i, 4);
-				cell.setCellStyle(styleMontant);
-				setText(cell, String.valueOf(operation.getMontant()));
+				getCellOperation(row, 3).setCellValue(operation.getMontant());
+			}
+			else {
+				getCellOperation(row, 4).setCellValue(operation.getMontant());
 			}
 
 			i++;
 		}
+	}
+
+	protected HSSFCell getCellOperation(HSSFRow row, int col) {
+		HSSFCell cell = row.createCell(col);
+		HSSFCellStyle style = row.getSheet().getWorkbook().createCellStyle();
+
+		if (row.getRowNum() % 2 == 0) {
+			style.setFillForegroundColor(HSSFColor.BLUE_GREY.index);
+		}
+		else {
+			style.setFillForegroundColor(HSSFColor.GREEN.index);
+		}
+		style.setFillPattern(CellStyle.BIG_SPOTS);
+		
+		cell.setCellStyle(style);
+		
+		return cell;
 	}
 }
---
+++ projet-banque/projet-banque-web/src/main/java/com/excilys/projet/banque/web/view/ExcelCompteView3.java
@@ -0,0 +1,96 @@
+package com.excilys.projet.banque.web.view;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jxl.Cell;
+import jxl.biff.EmptyCell;
+import jxl.write.DateTime;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+import org.springframework.web.servlet.view.document.AbstractJExcelView;
+
+import com.excilys.projet.banque.model.Compte;
+import com.excilys.projet.banque.model.Operation;
+
+public class ExcelCompteView3 extends AbstractJExcelView {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, WritableWorkbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		WritableSheet sheet;
+
+		int annee = (Integer) model.get("annee");
+		String mois = (String) model.get("mois");
+		Compte compte = (Compte) model.get("compte");
+		List<Operation> operations = (List<Operation>) model.get("operations");
+		float soustotal = (Float) model.get("soustotal");
+		float soustotalCarte = (Float) model.get("soustotalCarte");
+		float total = (Float) model.get("total");
+		
+		sheet = workbook.getSheet(0);
+		
+		
+		EmptyCell a = new EmptyCell(0, 0);
+		Label cell = (Label) sheet.getCell(6, 0);
+		cell.setString("aaaa");
+		
+		/*
+		addCell(sheet, 0, 0, "Détail de mon compte ("+mois+" "+annee+")");
+		addCell(sheet, 2, 2, compte.getLibelle());
+		addCellNumber(sheet, 3, 2, compte.getSolde());
+
+		int i = 6;
+		for (Operation operation : operations) {
+			addCellDate(sheet, i, 0, operation.getDateOp());
+			addCell(sheet, i, 1, operation.getLibelle());
+			addCell(sheet, i, 2, operation.getType().name());
+
+			if (operation.getMontant() < 0) {
+				addCellNumber(sheet, i, 3, operation.getMontant());
+			}
+			else {
+				addCellNumber(sheet, i, 4, operation.getMontant());
+			}
+
+			i++;
+		}
+		
+		addCell(sheet, i, 0, "Opérations");
+		addCellNumber(sheet, i, soustotal < 0 ? 3 : 4, soustotal);
+
+		i++;
+		addCell(sheet, i, 0, "Opérations par carte");
+		addCellNumber(sheet, i, soustotalCarte < 0 ? 3 : 4, soustotalCarte);
+
+		i++;
+		addCell(sheet, i, 0, "Total");
+		addCellNumber(sheet, i, total < 0 ? 3 : 4, total);
+		*/
+	}
+	
+	private Label addCell(WritableSheet sheet, int row, int col, String value) throws RowsExceededException, WriteException {
+		Label cell = new Label(row, col, value);
+		sheet.addCell(cell);
+		return cell;
+	}
+	private Number addCellNumber(WritableSheet sheet, int row, int col, double value) throws RowsExceededException, WriteException {
+		Number cell = new Number(row, col, value);
+		sheet.addCell(cell);
+		return cell;
+	}
+	private DateTime addCellDate(WritableSheet sheet, int row, int col, Date value) throws RowsExceededException, WriteException {
+		DateTime cell = new DateTime(row, col, value);
+		sheet.addCell(cell);
+		return cell;
+	}
+}
