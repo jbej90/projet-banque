35539592e11aae5aee95d83d20313cd3570b4a68
Elaaboudi.Meriem <elaaboudi.meriem@gmail.com>
2011-03-14 15:14:54 +0000

    commit du service web jax:ws

--- projet-banque/projet-banque-webservice/src/main/java/com/excilys/projet/banque/client/SoapClient.java
+++ projet-banque/projet-banque-webservice/src/main/java/com/excilys/projet/banque/client/SoapClient.java
@@ -1,5 +1,7 @@
 package com.excilys.projet.banque.client;
 
+import javax.xml.ws.BindingProvider;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,7 +15,10 @@ public class SoapClient {
 	public static void main(String args[]) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/context/applicationContext-Client.xml");
 		IWService iwServiceClient = applicationContext.getBean("client", IWService.class);
-		iwServiceClient.passerOperation(1, 2, 20);
+		BindingProvider provider = (BindingProvider) iwServiceClient;
+		provider.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "login");
+		provider.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "pass");
+		// iwServiceClient.passerOperation(1, 2, 20);
 	}
 
 }
