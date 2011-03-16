15e420607dc9aeced06175d2d43c91fc86074719
athomas.nc <athomas.nc@gmail.com>
2011-03-16 12:00:01 +0000

    Création d'un DTO pour la liste des comptes + mise à jour du web service REST + Refacto des DTO (Suppression des classes inutiles) + Création des tests soapUI pour REST

--- projet-banque/projet-banque-webservice/src/main/java/com/excilys/projet/banque/webservice/IWService.java
+++ projet-banque/projet-banque-webservice/src/main/java/com/excilys/projet/banque/webservice/IWService.java
@@ -1,21 +1,17 @@
 package com.excilys.projet.banque.webservice;
 
-import java.util.List;
-
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import com.excilys.projet.banque.webservice.dto.CompteDTO;
-import com.excilys.projet.banque.webservice.dto.OperationDTO;
+import com.excilys.projet.banque.webservice.dto.ComptesDTO;
 
 @WebService
 public interface IWService {
 
-	List<CompteDTO> consultationComptes(@WebParam(name = "idClient") int idClient);
+	ComptesDTO consultationComptes(@WebParam(name = "idClient") int idClient);
 
 	CompteDTO consultationCompte(@WebParam(name = "idClient") int idClient, @WebParam(name = "idCompte") int idCompte);
-
-	List<OperationDTO> consultationOperations(@WebParam(name = "idClient") int idClient, @WebParam(name = "idCompte") int idCompte);
 
 	String passerOperation(@WebParam(name = "idCompteEmetteur") int idCompteEmetteur, @WebParam(name = "idCompteDestinataire") int idCompteDestinataire,
 			@WebParam(name = "montant") float montant);
